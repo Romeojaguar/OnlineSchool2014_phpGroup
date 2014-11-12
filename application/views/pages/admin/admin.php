598007041a674ab1c14bc451808566ac0b6951f2
Romeojaguar <romeo.jaguar@mail.ru>
2014-11-12 12:20:49 +0300

    Update admin.php

--- application/views/pages/admin/admin.php
+++ application/views/pages/admin/admin.php
@@ -2,7 +2,7 @@
                           <div  class="modal-dialog ">
                                 <div class="modal-content">
 
-                                      <form  role="form" method="post" action="admin/addingus" name="form">
+                                      <form  role="form" method="post" action="addingus" name="form">
 
                                           <div id="registeruser">
 
@@ -26,14 +26,45 @@
                                 </div>
                           </div>
                     </div>
-                     <div class="alala">
+                    <div class="modal fade " id="myreguser" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div  class="modal-dialog ">
+                            <div class="modal-content">
+
+                                <form  role="form" method="post" action="admin/addingus" name="form">
+
+                                    <div id="registeruser">
+
+                                        <div class="header">
+
+
+                                        </div>
+                                        <div id="cbxclassregister">
+
+                                        <h1>Добавление расписание для класса <select id="styledSelect" class="form-control right" style=" margin-top: 8px;width: 11%;">
+                                                <option>231</option>
+                                                <option>123</option>
+                                            </select></h1>
+
+                                        </div>
+                                        <div class="rgstrcntr">
+
+                                            <input type="submit"  name='rdbtn' class="buttonregistr" value="Добавить">
+
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="alala">
                           <div id="admin_txt">
 
                               <ul>
 
                                   <a href="#myModal" role="button" data-toggle="modal"><li>Регистрация нового пользователя</li></a>
 
-                                  <li> Зарегистрированные пользователи</li>
+                                  <a href="admin/regusers" ><li> Зарегистрированные пользователи</li></a>
                               </ul>
 
                           </div>
@@ -41,7 +72,7 @@
                          <div id="admin_txt_t">
                                   <ul>
                                       <li class="cllaaaa">Расписание занятий</li>
-                                      <li class="clssss"> Добавление нового расписания</li>
+                                      <a href="#myreguser" role="button" data-toggle="modal"> <li class="clssss"> Добавление нового расписания</li></a>
                                       <li class="clssss"> Просмотр расписания</li>
 
                                   </ul>
